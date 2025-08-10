3cbea8422a061d5c64200b1349cb01d298eaa641
Debanjali081 <debanjali017@gmail.com>
2025-08-10 13:19:15 +0530

    play pause controller

--- src/controllers/spotifyController.js
+++ src/controllers/spotifyController.js
@@ -1,6 +1,34 @@
 import { spotifyApi } from "../utils/spotifyApi.js";
 import { getAccessToken } from "./authController.js";
 
+export async function getSpotifyData(req, res) {
+  try {
+    const api = spotifyApi(getAccessToken());
+    const [topTracks, nowPlaying, followedArtists] = await Promise.all([
+      api.get("/me/top/tracks?limit=10"),
+      api.get("/me/player/currently-playing"),
+      api.get("/me/following?type=artist")
+    ]);
+
+    res.json({
+      top_tracks: topTracks.data.items.map(track => ({
+        name: track.name,
+        artist: track.artists.map(a => a.name).join(", "),
+        uri: track.uri
+      })),
+      now_playing: nowPlaying.data?.item
+        ? {
+            name: nowPlaying.data.item.name,
+            artist: nowPlaying.data.item.artists.map(a => a.name).join(", ")
+          }
+        : "Nothing is playing",
+      followed_artists: followedArtists.data.artists.items.map(a => a.name)
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
 export async function playTrack(req, res) {
   const { uri } = req.body;
 
